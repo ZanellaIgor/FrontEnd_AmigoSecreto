1b5424bc06ac4f79f5390b7a4e74d50b61815568
ZanellaIgor <igor_zanella@outlook.com>
2024-04-12 00:32:07 -0300

    Iniciado Parte Administrativa

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,4 +1,9 @@
-const Page = () => {
+import { redirect } from 'next/navigation';
+import { pingAdmin } from '../components/api/server';
+
+const Page = async () => {
+  const logged = await pingAdmin();
+  if (!logged) return redirect('/admin/login');
   return (
     <div>
       <h1 className="text-center text-3xl">Admin</h1>
